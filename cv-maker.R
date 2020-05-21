afcecefe94098f2a48dae03109842da4c262f64c
Subhayan Mukerjee <mail@subhayan.com>
2020-05-21 16:21:32 -0500

    cv files

---
+++ cv-maker.R
@@ -0,0 +1,10 @@
+setwd("C:/Users/Subhayan/Documents/Work/cv/")
+
+# run ?datadrivencv::use_datadriven_cv to see more details
+datadrivencv::use_datadriven_cv(
+  full_name = "Subhayan Mukerjee",
+  data_location = "https://docs.google.com/spreadsheets/d/1iNLvXGwg4sx3mWt_L1_IDwTImd7fSqi-ynM66TUjbrs/edit?usp=sharing",
+  pdf_location = "https://github.com/wrahool/cv/final/mukerjee_cv.pdf",
+  html_location = "",
+  source_location = "https://github.com/wrahool/cv"
+)
